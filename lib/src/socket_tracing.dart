6e976efa18aa8b989fabc572ef59798239c76d96
Manuel Pineyro <mopineyro@gmail.com>
2021-02-01 20:21:58 -0500

    more tracing edits

--- lib/src/socket_tracing.dart
+++ lib/src/socket_tracing.dart
@@ -4,80 +4,82 @@ import 'package:phoenix_socket/phoenix_socket.dart';
 /// Callback for external reporting purposes on latency measurement. E.g. Send to datadog.
 typedef ReportingCallback = void Function(String topic, int measurement);
 
-// /// Socket Tracing Options
-// class SocketTracingOptions {
-//   /// Init options for Tracing
-//   SocketTracingOptions({this.trace = false, this.reportingCallback});
-
-//   /// Enable / disable tracing.
-//   final bool trace;
-
-//   /// Function to report back spans.
-//   final ReportingCallback reportingCallback;
-// }
-
 /// Used to measure socket latency
 class SocketTracing {
   /// Can be initialized with reporting callback to report back measurements.
   SocketTracing({this.enabled = false, ReportingCallback reportingCallback}) {
     _reportingCallback = reportingCallback;
     _logger = Logger('phoenix_socket.latency');
+    _latenciesPerChannel = {};
+    _awaitingMsgSpanStart = {};
   }
 
   /// Is socket tracing enabled?
   bool enabled;
 
-  /// Tracked message refs aaiting response from server.
-  Map<String, int> awaitingMsgSpanStart = {};
-
-  /// List of current session latencies per channel.
-  Map<String, List<int>> latenciesPerChannel;
-
+  Map<String, int> _awaitingMsgSpanStart;
+  Map<String, List<int>> _latenciesPerChannel;
   Logger _logger;
   ReportingCallback _reportingCallback;
 
-  String _channelName(String topic) => topic.replaceAll(
+  String _parseChannelName(String topic) => topic.replaceAll(
       RegExp(
         '[:,*&?!@#\$%]',
       ),
       '');
 
+  String _channelName(Message message) {
+    final channelName = _parseChannelName(message.topic);
+
+    if (channelName == 'phoenix') {
+      // return message.event.value;
+      // TODO: store the 'topic' on startSpan in _awaitingMsgSpanStart.
+      return 'heartbeat';
+    }
+
+    return channelName;
+  }
+
   /// Begin latency mesaurement.
   void startSpan(Message message) {
     final latencySpanStart = DateTime.now().millisecondsSinceEpoch;
-    final channelName = _channelName(message.topic);
+    final channelName = _channelName(message);
 
-    _logger.finer(() =>
-        'Channel: $channelName | Send Time: ${latencySpanStart}ms | Ref: ${message.ref}');
+    _logger.finer(() {
+      final channelText =
+          channelName == 'heartbeat' ? 'Heartbeat' : 'Channel: $channelName';
 
-    awaitingMsgSpanStart[message.ref] = latencySpanStart;
+      return '$channelText | Send Time: ${latencySpanStart}ms | Ref: ${message.ref}';
+    });
+
+    _awaitingMsgSpanStart[message.ref] = latencySpanStart;
   }
 
   /// End latency measurement.
   void endSpan(Message message) {
-    if (awaitingMsgSpanStart.containsKey(message.ref)) {
+    if (_awaitingMsgSpanStart.containsKey(message.ref)) {
       final latencySpanEnd = DateTime.now().millisecondsSinceEpoch;
-      final latencySpanStart = awaitingMsgSpanStart[message.ref];
+      final latencySpanStart = _awaitingMsgSpanStart[message.ref];
       final measuredLatency = latencySpanEnd - latencySpanStart;
-      final channelName = _channelName(message.topic);
+      final channelName = _channelName(message);
 
-      awaitingMsgSpanStart.remove(message.ref);
+      _awaitingMsgSpanStart.remove(message.ref);
 
-      if (latenciesPerChannel[channelName] != null) {
-        latenciesPerChannel[channelName] = [];
+      if (_latenciesPerChannel.containsKey(channelName)) {
+        _latenciesPerChannel[channelName].add(measuredLatency);
+      } else {
+        _latenciesPerChannel[channelName] = [measuredLatency];
       }
 
-      latenciesPerChannel[channelName].add(measuredLatency);
-
       final avgChannelLatency =
-          latenciesPerChannel[channelName].reduce((v, e) => v + e) /
-              latenciesPerChannel[channelName].length;
+          _latenciesPerChannel[channelName].reduce((v, e) => v + e) /
+              _latenciesPerChannel[channelName].length;
 
       _logger.finer(() =>
           'Channel: $channelName | Ref: ${message.ref} | End Time: ${latencySpanEnd}ms | Round Trip Time:  ${measuredLatency}ms | Avg. Channel Latency: ${avgChannelLatency}ms');
 
       if (_reportingCallback != null) {
-        final channel = _channelName(message.topic);
+        final channel = _channelName(message);
         _reportingCallback(channel, measuredLatency);
       }
     }
